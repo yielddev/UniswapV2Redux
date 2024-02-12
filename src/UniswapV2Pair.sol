6a94384b4c9b7be2f8ea1db0dd1a8374710a7e7f
yielddev <80984045+yielddev@users.noreply.github.com>
2024-02-12 11:35:17 -0700

    minor changes

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -73,13 +73,11 @@ contract UniswapV2Pair is IUniswapV2Pair, ERC20{
     }
 
     function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
-        // check this
-        // require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'UniswapV2: OVERFLOW');
         uint32 blockTimestamp = uint32(block.timestamp % 2**32);
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
         if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
+            // overflow is desired, thus unchecked
             unchecked {
-
                 price0CumulativeLast += uint(UQ112x112.encode(_reserve1).uqdiv(_reserve0)) * timeElapsed;
                 price1CumulativeLast += uint(UQ112x112.encode(_reserve0).uqdiv(_reserve1)) * timeElapsed;
             }
